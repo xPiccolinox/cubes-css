94da7d4601815cf90d3c38fa421fc4dd42ffc86d
Jakub Sobór <kupkeu@gmail.com>
2022-06-21 22:35:19 +0200

    Feat: So I made a cube

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cube1 from './Cube1/Cube1'
+
+function App() {
+  return (
+    <div className="App">
+      <Cube1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cube1/Cube1.js
@@ -0,0 +1,16 @@
+import './Cube1.css'
+
+const Cube1 = () => {
+  return (
+    <div id="cube">
+      <div className="cubeSide" />
+      <div className="cubeSide" />
+      <div className="cubeSide" />
+      <div className="cubeSide" />
+      <div id="cubeTop" />
+      <div id="cubeBottom" />
+    </div>
+  )
+}
+
+export default Cube1
