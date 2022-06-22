4c781db1852d4081d95a5b4d9ca34ea96d4dfda5
Jakub Sobór <kupkeu@gmail.com>
2022-06-22 22:31:09 +0200

    Feat: Cube now remembers previous rotation parameters; Change: Relocated all the functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import { useState } from 'react'
 import './App.css';
 import Cube1 from './Cube1/Cube1'
 
 function App() {
+  const [cursorOverCube, setCursorOverCube] = useState(false)
+  const [cursorGrabbingCube, setCursorGrabbingCube] = useState(false)
+  const [cursorCurrentPosition, setCursorCurrentPosition] = useState(0)
+  const [cursorGrabPosition, setCursorGrabPosition] = useState(0)
+  const [cubeLastRotation, setCubeLastRotation] = useState(0)
+
+  const handleOnMouseOver = () => {
+    setCursorOverCube(true)
+  }
+  const handleOnMouseOut = () => {
+    setCursorOverCube(false)
+  }
+  const handleOnMouseDown = (e) => {
+    setCursorGrabbingCube(true)
+    setCursorGrabPosition(e.clientX)
+  }
+  const handleOnMouseUp = () => {
+    setCursorGrabbingCube(false)
+    if (cursorGrabbingCube == true) {
+      setCubeLastRotation(cubeLastRotation - (cursorGrabPosition - cursorCurrentPosition) / 3)
+    }
+  }
+  const handleOnMouseMove = (e) => {
+      setCursorCurrentPosition(e.clientX)
+  }
+
+  if (cursorGrabbingCube == true) {
+    document.querySelector('body').style.cursor = 'grabbing'
+    document.getElementById('cube').style.transform = `rotateX(-20deg) rotateY(${cubeLastRotation - (cursorGrabPosition - cursorCurrentPosition) / 3}deg)`
+  }
+  else if (cursorOverCube == true && cursorGrabbingCube == false) {
+    document.querySelector('body').style.cursor = 'grab'
+  }
+  else {
+    document.querySelector('body').style.cursor = 'auto'
+  }
+  if (Math.abs(cubeLastRotation) >= 360) {
+    setCubeLastRotation(cubeLastRotation % 360)
+  }
+
   return (
-    <div className="App">
-      <Cube1 />
+    <div className="App" onMouseUp={handleOnMouseUp} onMouseMove={handleOnMouseMove}>
+      <Cube1 
+      handleOnMouseOver={handleOnMouseOver}
+      handleOnMouseOut={handleOnMouseOut}
+      handleOnMouseDown={handleOnMouseDown} />
     </div>
   );
 }
--- src/Cube1/Cube1.js
+++ src/Cube1/Cube1.js
@@ -1,40 +1,12 @@
 import { useState, useEffect } from 'react'
 import './Cube1.css'
 
-const Cube1 = () => {
-  const [cursorInsideCube, setCursorInsideCube] = useState(false)
-  const [cursorGrabbedCube, setCursorGrabbedCube] = useState(false)
-  const [cursorGrabPosition, setCursorGrabPosition] = useState([0, 0])
-  const [cursorCurrentPosition, setCursorCurrentPosition] = useState([0, 0])
-
-  useEffect(() => {
-    document.getElementById('cube').addEventListener('mouseover', () => {
-      setCursorInsideCube(true)
-    })
-    document.getElementById('cube').addEventListener('mouseout', () => {
-      setCursorInsideCube(false)
-    })
-    document.getElementById('cube').addEventListener('mousedown', (e) => {
-      setCursorGrabbedCube(true)
-      setCursorGrabPosition([e.clientX, e.clientY])
-    })
-    window.addEventListener('mouseup', () => {
-      setCursorGrabbedCube(false)
-    })
-    document.getElementById('cube').addEventListener('mousemove', (e) => {
-      setCursorCurrentPosition([e.clientX, e.clientY])
-    })
-  }, [])
-
-  if (cursorGrabbedCube == true) {
-    document.querySelector('body').style.cursor = 'grabbing'
-    document.getElementById('cube').style.transform = `rotateX(-20deg) rotateY(${(cursorGrabPosition[0] + cursorCurrentPosition[0]) / 3}deg)`
-  }
-  else if (cursorInsideCube == true && cursorGrabbedCube == false) document.querySelector('body').style.cursor = 'grab'
-  else document.querySelector('body').style.cursor = 'auto'
-
+const Cube1 = (props) => {
   return (
-    <div id="cube">
+    <div id="cube"
+    onMouseOver={props.handleOnMouseOver}
+    onMouseOut={props.handleOnMouseOut}
+    onMouseDown={props.handleOnMouseDown} >
       <div className="cubeSide" />
       <div className="cubeSide" />
       <div className="cubeSide" />
